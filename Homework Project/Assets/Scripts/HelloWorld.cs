0481726c87b4f8dedd5f69588f959f7ad0c5c676
Tiramisukatsu <SaintAtlas0001@gmail.com>
2024-09-06 22:01:22 -0400

    Homework Complete

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -8,9 +8,19 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello, world!");
+        Debug.LogWarning("! ! ! ! ! ! ! ! ! ! ! ! ! ! !");
+        Debug.LogError("You have done something terribly wrong");
     }
 
     // Update is called once per frame
+    // Author: Nick-Name
+    /*
+
+    I have no questions....yet
+    A game idea I have....is to create real, unpredicatable horror using an open-world map and beasts controlled by advanced AI, with a functional ecosystem like in Rain World
+    A game development area I hope to learn more about is programming xd 
+
+     */
     void Update()
     {
         
